931c6e6a4b9e2448cdc1925e2f1ad9f804d44ee3
Paveł <Paulik0182@gmail.com>
2022-04-03 15:20:08 +0300

    ViewHolder -> создаем NoteViewHolder

--- app/src/main/java/com/android/note/ui/NoteAdapter.java
+++ app/src/main/java/com/android/note/ui/NoteAdapter.java
@@ -6,35 +6,43 @@ package com.android.note.ui;
  * - getItemCount
  */
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.note.R;
 import com.android.note.domain.NoteEntity;
 
 import java.util.List;
 
-public class NoteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
 
     private List<NoteEntity> data;
 
     //создание ViewHolder (создаем view)
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        //создаем NoteViewHolder
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_note, parent, false);
+        NoteViewHolder viewHolder = new NoteViewHolder(itemView);
+        return viewHolder;
+    }
 
     //к имеющимуся holder подставить данные
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
 
         final NoteEntity noteEntity = data.get(position);//получили данные
-        //todo положить в holder
-    }
 
+        holder.titleTextView.setText(noteEntity.getTitle());
+        holder.contentTextView.setText(noteEntity.getContent());
+    }
 
     //вернуть количество данных
     @Override
