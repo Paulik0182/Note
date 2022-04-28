049ac0ddf48fd0b020eff2bff23aa1f25d02db1e
Paveł <Paulik0182@gmail.com>
2022-04-28 17:27:36 +0300

    Activity -> transfer -> Fragment. Создание фрагментов.

---
+++ app/src/main/java/com/android/note/ui/NotListFragment.java
@@ -0,0 +1,46 @@
+package com.android.note.ui;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.note.R;
+
+public class NotListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public static NotListFragment newInstance(String param1, String param2) {
+        NotListFragment fragment = new NotListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_not_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setBackgroundColor(Color.GREEN); //пример, демонстрация работы, окрасили view в цвет
+    }
+}
---
+++ app/src/main/java/com/android/note/ui/RootActivity.java
@@ -0,0 +1,23 @@
+package com.android.note.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.android.note.R;
+
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+
+        Fragment noteListFragment = new NotListFragment(); //создали фрагмент
+        getSupportFragmentManager()
+                .beginTransaction() //начать транзакцию
+                .add(R.id.container_layout, noteListFragment) //здесь мы укакзываем в какой layout мы вставляем фрагмент
+                .commit(); //закончить транзакцию
+    }
+}
